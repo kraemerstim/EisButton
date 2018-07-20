267a7373ad99383d79b3d67036cc7b55b6e6f8d3
Tim Kraemer <kraemerstim@gmail.com>
2018-07-20 08:52:56 +0200

    Eisbutton-programm updated to the box included time check

--- ESPEisbutton/ESPEisbutton.ino
+++ ESPEisbutton/ESPEisbutton.ino
@@ -5,7 +5,7 @@
 #include "ConnectionDetails.h"
 WiFiClient client;
 
-int inputPin = D0;
+int inputPin = D2;
 int ledPin = D4;
 int buttonState = 0;
 
@@ -27,13 +27,13 @@ void setup() {
   pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
   Serial.println("starting");
-  WiFi.disconnect();
+  wifiConnect();
 }
 
 String get_current_time() {
   if (WiFi.status() != WL_CONNECTED) {
     //WIFI Muss verbunden sein!
-    return "Fail!";
+    return "00:00";
   }
   HTTPClient http;
   http.begin("http://worldclockapi.com/api/json/cet/now");
@@ -45,16 +45,42 @@ String get_current_time() {
   }
 }
 
-void send_hipchat_notification(String text) {
+int getTotalMinutes(const char* timeToTransform) {
+  int hh, mm, minutes;
+  char s[255];
+  sscanf(timeToTransform, "%d:%d",&hh,&mm);
+  minutes = hh*60+mm;
+  sprintf(s, "time: %s minutes: %d", timeToTransform, minutes);
+  Serial.println(s);
+  return minutes;
+}
+
+boolean isValidTime(const char* currentTime) {
+  int cur, first, last;
+  cur = getTotalMinutes(currentTime);
+  first = getTotalMinutes(ACTIVATE_TIME);
+  last = getTotalMinutes(DEACTIVATE_TIME);
+
+  if (cur > last || cur < first) {
+    return false;
+  }
+  return true;
+}
+
+void send_hipchat_notification(boolean isIceTime) {
   HTTPClient http;
   char buf[256];
-  snprintf(buf, sizeof buf, "https://cassoftware.hipchat.com/v2/room/%s/notification?auth_token=%s", HIPCHAT_CHANNEL, HIPCHAT_TOKEN);
+  if (isIceTime) {
+    snprintf(buf, sizeof buf, "https://cassoftware.hipchat.com/v2/room/%s/notification?auth_token=%s", HIPCHAT_CHANNEL_EIS, HIPCHAT_TOKEN_EIS);
+  } else {
+    snprintf(buf, sizeof buf, "https://cassoftware.hipchat.com/v2/room/%s/notification?auth_token=%s", HIPCHAT_CHANNEL_TEST, HIPCHAT_TOKEN_TEST);
+  }
   http.begin(buf, HIPCHAT_THUMBPRINT);
   http.addHeader("Content-Type", "application/json");
   
   DynamicJsonBuffer jsonBuffer(400);
   JsonObject& root = jsonBuffer.createObject();
-  root["message"] = text;
+  root["message"] = "(eismann)";
   root["notify"] = "true";
   root["message_format"] = "text";
   root["color"] = "random";
@@ -75,20 +101,18 @@ void send_hipchat_notification(String text) {
 
 void loop() {
   buttonState = digitalRead(inputPin);
-  if (buttonState == 0)
+  if (buttonState == 1)
   {
     digitalWrite(ledPin, LOW); 
-    wifiConnect();
-    String current_time = get_current_time();
+    const char* current_time = get_current_time().c_str();
     Serial.println(current_time);
     delay(300);
-    send_hipchat_notification(current_time);
-    WiFi.disconnect();
-    Serial.println("disconnected");
+    send_hipchat_notification(isValidTime(current_time));
+    Serial.println("send to Hipchat");
     digitalWrite(ledPin, HIGH); 
     delay(10000);
   }
-  if (buttonState == 1)
+  if (buttonState == 0)
   {
     delay(50);
   }
